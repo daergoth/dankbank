76c33be9e329b8f7c1b09c2fd6a2bc9fb84c9157
Attila Bagossy <bagossyattila@outlook.com>
2017-06-21 21:50:49 +0200

    Small stylistic modifications in ShareActivity

--- app/src/main/java/net/daergoth/dankbank/ShareActivity.java
+++ app/src/main/java/net/daergoth/dankbank/ShareActivity.java
@@ -4,20 +4,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 
 /**
- * Created by Attila on 2017. 06. 21..
+ * @author Attila Bagossy
+ * @author Petya Lakatos
  */
-
 public class ShareActivity extends AppCompatActivity {
 
     private ImageView imageShow;
