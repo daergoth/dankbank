ae056f14e746526679dedc7bbb566a80262f944b
Attila Bagossy <bagossyattila@outlook.com>
2017-06-22 00:37:52 +0200

    Implemented DAO save functionality

--- app/src/main/java/net/daergoth/dankbank/meme/MemeDaoImpl.java
+++ app/src/main/java/net/daergoth/dankbank/meme/MemeDaoImpl.java
@@ -1,9 +1,11 @@
 package net.daergoth.dankbank.meme;
 
 import android.net.Uri;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 import net.daergoth.dankbank.tag.Tag;
 import net.daergoth.dankbank.tag.TagDao;
@@ -11,6 +13,8 @@ import net.daergoth.dankbank.tag.TagDao;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -56,7 +60,7 @@ public class MemeDaoImpl implements MemeDao {
 
     @Override
     public List<Meme> getMemesByTag(final Tag t) {
-        List<Meme> memesWithGivenTag = new ArrayList<>();
+        final List<Meme> memesWithGivenTag = new ArrayList<>();
 
         for (Meme m : cachedMemes) {
             if(m.getTags().contains(t)) {
@@ -68,10 +72,16 @@ public class MemeDaoImpl implements MemeDao {
     }
 
     private void saveChanges() {
+        final StoredMemes storedMemes = convertToStoredMemes(this.cachedMemes);
 
+        try {
+            final JsonWriter jsonWriter = new JsonWriter(new FileWriter(saveFile));
+
+            gson.toJson(storedMemes, StoredMemes.class, jsonWriter);
+        } catch (IOException e) {
+            Log.e(MemeDaoImpl.class.getName(), e.getMessage());
+        }
     }
-
-
 
     private List<Meme> loadMemes() {
         if (!saveFile.exists()) {
@@ -83,7 +93,7 @@ public class MemeDaoImpl implements MemeDao {
 
             final StoredMemes storedMemes = gson.fromJson(jsonReader, StoredMemes.class);
 
-            return convertStoredMemes(storedMemes);
+            return convertFromStoredMemes(storedMemes);
         } catch (FileNotFoundException e) {
             // this branch should not be reached at all
 
@@ -91,7 +101,31 @@ public class MemeDaoImpl implements MemeDao {
         }
     }
 
-    private List<Meme> convertStoredMemes(StoredMemes storedMemes) {
+    private StoredMemes convertToStoredMemes(List<Meme> memes) {
+        final StoredMemes storedMemes = new StoredMemes();
+
+        final List<StoredMeme> storedMemeList = new ArrayList<>();
+
+        for (Meme meme : memes) {
+            final StoredMeme storedMeme = new StoredMeme();
+
+            storedMeme.setUri(meme.getUri().toString());
+
+            final List<Integer> tagIds = new ArrayList<>();
+
+            for (Tag tag : meme.getTags()) {
+                tagIds.add(tag.getId());
+            }
+
+            storedMeme.setTagIds(tagIds);
+        }
+
+        storedMemes.setMemes(storedMemeList);
+
+        return storedMemes;
+    }
+
+    private List<Meme> convertFromStoredMemes(StoredMemes storedMemes) {
         final List<Meme> result = new ArrayList<>();
 
         for (StoredMeme storedMeme : storedMemes.getMemes()) {
--- app/src/main/java/net/daergoth/dankbank/tag/TagDaoImpl.java
+++ app/src/main/java/net/daergoth/dankbank/tag/TagDaoImpl.java
@@ -1,13 +1,18 @@
 package net.daergoth.dankbank.tag;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
 import net.daergoth.dankbank.meme.MemeDaoImpl;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -56,9 +61,18 @@ public class TagDaoImpl implements TagDao {
     }
 
     private void saveChanges() {
+        final StoredTags storedTags = new StoredTags();
 
+        storedTags.setTags(new ArrayList<Tag>(this.cachedTags.values()));
+
+        try {
+            final JsonWriter jsonWriter = new JsonWriter(new FileWriter(saveFile));
+
+            gson.toJson(storedTags, StoredTags.class, jsonWriter);
+        } catch (IOException e) {
+            Log.e(TagDaoImpl.class.getName(), e.getMessage());
+        }
     }
-
 
     private Map<Integer, Tag> loadTags() {
         if (!saveFile.exists()) {
